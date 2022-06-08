b0f1503f34825165c6fc71500fc258dddc6dd086
Kazi Shariful Islam <kazisharifulislam52@gmail.com>
2022-06-08 08:18:29 +0600

    go start

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
+
 import "fmt"
-func main(){
-	fmt.Println("Hello World");
+
+func main() {
+	// fmt.Print("Hello World. ")
+	// //  escape sequence \n
+	// fmt.Println("Hello \nBangladesh")
+	// fmt.Print("I'm Kazi Shariful Islam.\n A passionate web developer.\n")
+	// fmt.Println("name \t \t \t age")
+	// fmt.Print("Sharifiul Islam \t 23")
+
+	fmt.Println("\"Kazi Shariful \\ islam\"")
+
 }
